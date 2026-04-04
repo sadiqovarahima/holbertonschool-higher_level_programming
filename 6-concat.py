0d7afa1cef590f22903194dd5d0f22f8609a388d
sadiqovarahima <13726@holbertonstudents.com>
2026-04-04 16:33:36 +0000

    finish

--- 6-concat.py
+++ 6-concat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str2 = "Holberton School"
-str1 = "Welcome "
-print(f"{str1}" + "to" + f"{str2}" + "!")
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + str2
+print(f"Welcome to {str1}!")
