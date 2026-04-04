0d5a48c59773229e6a138c6a4409335b65092a18
sadiqovarahima <13726@holbertonstudents.com>
2026-04-04 18:15:58 +0000

    sdgf

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interavtive, object-oriented programming\
+language that combines remarkable power with very claer syntax"
+str=str[39:66] + " with Python"
+print(str)
