9cdaba38d0df54e5d13d670d3da7087d50d549a9
sadiqovarahima <13726@holbertonstudents.com>
2026-04-17 12:24:56 +0000

    fix

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        max_integer = my_list[0]
+        for digit in my_list:
+            if digit > max_integer:
+                max_integer = digit
+        return  max_integer
