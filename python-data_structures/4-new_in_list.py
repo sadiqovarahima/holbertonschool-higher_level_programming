367e6eff18b7268c28faeb8702f6b4f95bfcadfc
sadiqovarahima <13726@holbertonstudents.com>
2026-04-17 11:21:41 +0000

    fix

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     copy_list = my_list[:]
     if idx < 0 or idx >= len(my_list):
-       return copy_list
+        return copy_list
     copy_list[idx] = element
     return copy_list
