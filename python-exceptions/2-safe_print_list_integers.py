7881ce450874a301613eaf4409dd4a1baf4a2856
sadiqovarahima <13726@holbertonstudents.com>
2026-04-17 16:07:45 +0000

    fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(element), end="")
                 count += 1
             except (ValueError, TypeError):
-                continue
+                pass
         except IndexError:
             break
     print("")
