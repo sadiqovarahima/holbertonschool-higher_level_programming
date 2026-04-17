b6734a259340f36fd1e8d30475c9ac9d663649d3
sadiqovarahima <13726@holbertonstudents.com>
2026-04-17 15:25:27 +0000

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,11 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     roman_dict = {
-            "I" : 1, "V" : 5, "X" : 10, "L" : 50,
-            "C" : 100, "D" : 500, "M" : 1000
+            "I": 1, "V": 5, "X": 10, "L": 50,
+            "C": 100, "D": 500, "M": 1000
             }
     total = 0
-    n=len(roman_string)
+    n = len(roman_string)
     for i in range(n):
         current = roman_dict.get(roman_string[i], 0)
         if i + 1 < n and current < roman_dict.get(roman_string[i + 1], 0):
