3c30b683d28660182565e41bd7f4bbbf958c6b04
sadiqovarahima <13726@holbertonstudents.com>
2026-04-17 14:19:36 +0000

    fix

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = []
-    for i in my_list:
-        my_list.count(i) > 1:
-            new_list.append(i)
-    return new_list
+    uniq = set(my_list)
+    for i in uniq:
+        total += i
+    return total
