21abec571e185aa6d28ae518ddd6cce8cad4ea36
sadiqovarahima <13726@holbertonstudents.com>
2026-04-24 16:50:10 +0000

    fixx

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""bu bir basegeometry klassidir."""
+
+
+class BaseGeometry:
+    """basegeometry klass"""
+
+    def area(self):
+        """sahenin hesablanmasi"""
+        raise Exception("area() is not implemented")
