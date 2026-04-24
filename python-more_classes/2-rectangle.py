6b0198b090a878e8ca955550894f472d3d61f14f
sadiqovarahima <13726@holbertonstudents.com>
2026-04-24 13:34:59 +0000

    fix

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -36,6 +36,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+    
     def area(self):
         """saheni qaytaririq"""
         return self.__width * self.__height
