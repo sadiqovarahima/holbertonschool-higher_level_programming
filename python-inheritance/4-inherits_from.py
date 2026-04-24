1759ee750c6c80a8363e9bf926b23c4700e2689a
sadiqovarahima <13726@holbertonstudents.com>
2026-04-24 16:34:13 +0000

    fixx

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""obyektin mirsa yolu ile gelib-gelmediyini yoxlayir"""
+
+
+def inherits_from(obj, a_class):
+    """obyektin klassdan olub-olmadigini yoxlayir"""
+    return  issubclass(type(obj), a_class) and type(obj) is not a_class
