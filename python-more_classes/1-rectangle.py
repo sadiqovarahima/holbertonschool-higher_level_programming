c621dde335a92e99a0c5da9f45866c01c8d27542
sadiqovarahima <13726@holbertonstudents.com>
2026-04-24 12:27:09 +0000

    fix

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""senedlesme"""
+
+
+class Rectangle:
+    """duzbucaqlinin klasi"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    @property
+    def width(self):
+        """enini geri qaytaririq"""
+        return self.width
+    @width.setter
+    def width(self, value):
+        """eni teyin edir"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("witdh must be >= 0")
+        self.__width = value
+    @property
+    def height(self):
+        """hundurluyunu qaytarir"""
+        return self.height
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
