78d7ea621991e2abf56ef108642b70e2aaf642ce
sadiqovarahima <13726@holbertonstudents.com>
2026-04-24 16:01:27 +0000

    ifx

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""inheritance istifadesi"""
+
+
+class MyList(list):
+    """klass"""
+    
+    def print_sorted(self):
+        """artan sira ile cap edir"""
+        print(sorted(self))
