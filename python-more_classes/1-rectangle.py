a05d029b4dcd6d3e12c1e8231bb792e47aeb1eb9
sadiqovarahima <13726@holbertonstudents.com>
2026-04-24 12:39:52 +0000

    fix

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,12 +21,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
-    @property
+
+        @property
     def height(self):
         """hundurluyunu qaytarir"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
