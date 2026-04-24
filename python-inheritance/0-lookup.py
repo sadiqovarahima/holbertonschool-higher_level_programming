9393e80dcc4844d3e54c703b91a4e9c2b6c49175
sadiqovarahima <13726@holbertonstudents.com>
2026-04-24 15:55:53 +0000

    ifx

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""bu modul atribut ve metidlarin axtarmaq ucun lookup funksiyasini temin edir"""
+"""bu modul atribut ve metidlarin axtarmaq ucun lookup funksiyasi"""
 
 
 def lookup(obj):
