c8024cb0e9cc9ad42141f91668a29d18e151b963
sadiqovarahima <13726@holbertonstudents.com>
2026-04-24 16:58:21 +0000

    fix

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""sahenin hesablanmasi"""
+
+
+class BaseGeometry:
+    """hendesi fiqurlarin sehesi"""
+    
+    def area(self):
+        """saheni hesablayan metod"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """deyerin tam qiymet oldugunu yoxlayir"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
