bdb37b67d32921e32d622fbe89eacf8b51a44bfe
sadiqovarahima <13726@holbertonstudents.com>
2026-04-25 16:39:55 +0000

    fi

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Rectangleden miras alan square klassi"""
+Rectangle = __import__("9-rectangle.py").Rectangle
+
+
+class Square(Rectangle):
+    """"rectangleden square yaradilmasi"""
+
+    def __init__(self, size):
+        """kvadratin yaradilmasi"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """kvadratin sahesini qayatrir"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """string kimi qaytarilmasi"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
