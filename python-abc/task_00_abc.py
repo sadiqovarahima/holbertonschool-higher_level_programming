c11d111ab0043354fe4fdc9e0af8f3e61f3965f7
sadiqovarahima <13726@holbertonstudents.com>
2026-04-25 17:34:18 +0000

    fix

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""ABC class ANimal"""
 from abc import ABC, abstractmethod
 
 
@@ -10,18 +11,14 @@ class Animal(ABC):
         """Heyvanin cixardigi ses"""
         pass
 
-
 class Dog(Animal):
     """dig klassi animaldan miras alir"""
-
     def sound(self):
         """itin sesini qaytarir"""
         return "Bark"
 
-
 class Cat(Animal):
     """cat klasi animaldan miras alir"""
-
     def sound(self):
         """pisik sesi qayatrir"""
         return "Meow"
