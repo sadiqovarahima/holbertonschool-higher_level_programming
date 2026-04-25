c7739121199cbcb2ed43b7a8593e52cef0f7e13c
sadiqovarahima <13726@holbertonstudents.com>
2026-04-25 16:16:41 +0000

    fix

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -18,4 +18,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """string teqdimatini qaytarir"""
-        return "[Rectangle {}/{}".format(self.__width, self.__height)
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
