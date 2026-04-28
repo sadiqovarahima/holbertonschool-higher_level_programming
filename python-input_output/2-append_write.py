2b3d0865e647f56ee5d7d03983216622a864ecc0
sadiqovarahima <13726@holbertonstudents.com>
2026-04-28 12:48:13 +0000

    fix

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""Bu modul faylın sonuna mətn əlavə edən funksiyanı ehtiva edir."""
+
+
+def append_write(filename="", text=""):
+    """
+    Mətni UTF8 faylın sonuna əlavə edir və əlavə olunan simvolların sayını qaytarır.
+    """
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)
