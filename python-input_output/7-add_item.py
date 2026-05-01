74562dc0f7c0303515d39fc3ba27eeebaa9f3bce
sadiqovarahima <13726@holbertonstudents.com>
2026-05-01 13:25:51 +0000

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""add item"""
 import sys
 import os
+
 
 # Əvvəlki tapşırıqlardakı funksiyaları import edirik
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
