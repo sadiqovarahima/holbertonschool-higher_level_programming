f63d66f3cd0fc1bfe789c832c104f6e0d8cc1d6e
sadiqovarahima <13726@holbertonstudents.com>
2026-05-01 13:56:56 +0000

    fix

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""student to json with filter"""
+
+
+class Student:
+    """Filtrləmə qabiliyyəti olan tələbə klası"""
+
+    def __init__(self, first_name, last_name, age):
+        """Obyekt yaradılarkən atributları mənimsədir"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Obyektin lüğət təsvirini qaytarır.
+        Əgər attrs siyahısı verilirsə, yalnız oradakı atributları qaytarır.
+        """
+        # Bütün atributları lüğət kimi götürürük
+        my_dict = self.__dict__
+
+        # Əgər attrs siyahıdırsa və içindəkilər stringdirsə filtr tətbiq edirik
+        if isinstance(attrs, list) and all(isinstance(s, str) for s in attrs):
+            # Yalnız attrs siyahısında olan açarları saxlayan yeni lüğət yaradırıq
+            filtered_dict = {}
+            for key in attrs:
+                if key in my_dict:
+                    filtered_dict[key] = my_dict[key]
+            return filtered_dict
+
+        # Əgər attrs yoxdursa və ya formatı düz deyilsə, bütün lüğəti qaytarırıq
+        return my_dict
