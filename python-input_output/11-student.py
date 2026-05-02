d7f27717378f856a9812453cf7ed18c109e12594
sadiqovarahima <13726@holbertonstudents.com>
2026-05-02 13:10:36 +0000

    fix

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Student to disk and reload"""
+
+
+class Student:
+    """Defines a student by first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        If attrs is a list of strings, only those attributes are retrieved.
+        """
+        if isinstance(attrs, list) and all(isinstance(s, str) for s in attrs):
+            res = {}
+            for k in attrs:
+                if hasattr(self, k):
+                    res[k] = getattr(self, k)
+            return res
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance from a dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
