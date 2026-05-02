3492f08f26979ac58afe0ada1d52525b2759df1d
sadiqovarahima <13726@holbertonstudents.com>
2026-05-02 13:48:22 +0000

    fix

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -6,9 +6,10 @@ import json
 def serialization_and_save_to_file(data, filename):
     """json formatina cevirir ve fayla yzir"""
     with open(filename, "w") as f:
-        json.dump(data, f):
+        f.write(json.dumps(data))
 
 def load_and_deserialize(filename):
     """json faylini oxuyur"""
     with open(filename, "r") as f:
-        return json.load(f)
+        data = f.read()
+        return json.loads(data)
