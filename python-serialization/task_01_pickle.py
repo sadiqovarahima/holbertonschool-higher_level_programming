1492d30b69123e139e1aea80e58892911a10b002
sadiqovarahima <13726@holbertonstudents.com>
2026-05-02 14:09:05 +0000

    fix

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -11,22 +11,22 @@ class CustomObject:
         self.age = age
         self.is_student = is.student
 
-def serialize(self, filename):
-    with open(filename, "wb") as f:
-        pickle.dump(self, f)
+    def serialize(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
 
-def display(self):
-    print(f"Name: {self.name}")
-    print(f"Age: {self.age}")
-    print(f"Is Student: {self.is_student}")
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
-@classmethod
-def deserialize(cls, filename):
-    try:
-        with open(filename, "rb") as f:
-             obj = pickle.load(f)
-            if isinstance(obj, cls):
-                return obj
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as f:
+                obj = pickle.load(f)
+                if isinstance(obj, cls):
+                    return obj
+                return None
+        except Exception:
             return None
-    except Exception:
-        return None
