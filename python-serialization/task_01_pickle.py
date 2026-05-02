2c2e19e18a4d83398b78fe034e97df3a3daf2159
sadiqovarahima <13726@holbertonstudents.com>
2026-05-02 14:10:10 +0000

    fix

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -9,7 +9,7 @@ class CustomObject:
     def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
-        self.is_student = is.student
+        self.is_student = is_student
 
     def serialize(self, filename):
         with open(filename, "wb") as f:
