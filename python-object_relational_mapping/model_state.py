32290a8d9ae69fef636409e0d9491ea8654edf74
sadiqovarahima <13726@holbertonstudents.com>
2026-05-16 20:29:38 +0000

    fix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Defines a State class and Base using SQLAlchemy"""
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
