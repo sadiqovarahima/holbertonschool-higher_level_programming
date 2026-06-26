c74d9e0dff34704f5a3eb896ea2e4f30d1b70b63
sadiqovarahima <13726@holbertonstudents.com>
2026-06-26 14:55:36 +0000

    fix 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Script that lists all cities of a given state from hbtn_0e_4_usa"""
+"""
+Script that takes in the name of a state as an argument
+and lists all cities of that state, using the database hbtn_0e_4_usa
+"""
 import MySQLdb
 import sys
 
@@ -12,15 +15,22 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
+
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT cities.name FROM cities "
-        "JOIN states ON cities.state_id = states.id "
-        "WHERE BINARY states.name = %s "
-        "ORDER BY cities.id ASC",
-        (sys.argv[4],)
-    )
+
+    # Ştat adına görə şəhərləri tapmaq üçün JOIN sorğusu qururuq
+    query = "SELECT cities.name FROM cities " \
+            "JOIN states ON cities.state_id = states.id " \
+            "WHERE BINARY states.name = %s ORDER BY cities.id ASC"
+    cursor.execute(query, (sys.argv[4],))
+
     rows = cursor.fetchall()
-    print(", ".join(row[0] for row in rows))
+
+    # Şəhərlərin adlarını bir listə yığırıq
+    cities = [row[0] for row in rows]
+
+    # Şəhərləri aralarında ", " olacaq şəkildə birləşdirib ekrana çıxarırıq
+    print(", ".join(cities))
+
     cursor.close()
     db.close()
