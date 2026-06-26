67ffa44239cc6ac84d13aaea41cac7576407d245
sadiqovarahima <13726@holbertonstudents.com>
2026-06-26 14:46:56 +0000

    fix task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Script that filters states by user input, safe from SQL injection"""
+"""
+Script that takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument, safe from MySQL injection
+"""
 import MySQLdb
 import sys
 
@@ -12,13 +15,17 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
+
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC",
-        (sys.argv[4],)
-    )
+
+    # %s istifadə edərək SQL injection-ın qarşısını tam alırıq
+    # execute funksiyasının ikinci arqumenti mütləq tuple (sys.argv[4],) olmalıdır
+    query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY states.id ASC"
+    cursor.execute(query, (sys.argv[4],))
+
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+
     cursor.close()
     db.close()
