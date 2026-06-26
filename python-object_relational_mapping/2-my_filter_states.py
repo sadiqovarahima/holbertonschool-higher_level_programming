05e2d6e428da43b8b8f9e3c224878751e42fd1d1
sadiqovarahima <13726@holbertonstudents.com>
2026-06-26 14:43:04 +0000

    fix

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Script that filters states by user input"""
+"""
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
+    # Verilənlər bazasına qoşulma (Port mütləq 3306 olmalıdır)
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -12,14 +16,20 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
+    
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(
-            sys.argv[4]
-        )
-    )
+    
+    # Şərtdə tələb olunan .format() metodundan istifadə edərək sorğunu qururuq
+    # SQL injection-dan qorunmaq və mətni düzgün oxumaq üçün '{}' dırnaq daxilində yazılmalıdır
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(sys.argv[4])
+    
+    cursor.execute(query)
+    
+    # Nəticələri ekrana çıxarırıq
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+        
+    # Bağlantıları bağlayırıq
     cursor.close()
     db.close()
